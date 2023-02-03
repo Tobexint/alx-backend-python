97318d67afb9033ad4fc72843a7b6073f8e9ed3b
tobexint <tobexint@gmail.com>
2023-02-03 04:22:46 +0000

    annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function sum_list which takes a list
+ input_list of floats as argument and returns their sum as a float
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floats
+    Args:
+        input_list (list): A list of floats
+    Returns:
+        float: The sum of the floats in the list
+    """
+    if not input_list:
+        return 0
+    else:
+        return sum(input_list)
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function make_multiplier that takes a
+ float multiplier as argument and returns a function that multiplies a float
+ by multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by a number
+    """
+
+    def multiplier_func(number: float) -> float:
+        """
+        multiplies a float by multipler
+        """
+        return multiplier * number
+
+    return multiplier_func
